5e65ddc74ccdc30c6adb6190541fa5bef8304273
Stanley Huang <huang.stanley050@gmail.com>
2020-01-11 14:19:24 +1100

    typo in last commit, fixed, push to remote again

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const authRouter = require("./routes/authRouter");
+const authRouter = require("./routes/authRoutes");
 
 app.get("/", (req, res) => res.send("Index route"));
 app.get("/hello", (req, res) => res.send("hello there"));
