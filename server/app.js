1c28c8215ccda34f56fb1436ddcdc3ac3252664a
Stanley Huang <huang.stanley050@gmail.com>
2020-01-11 14:33:00 +1100

    trying to serve react app and test heroku post build

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRoutes");
 
+app.use(express.static(__dirname + "../client/build"));
 app.get("/", (req, res) => res.send("Index route"));
 app.get("/hello", (req, res) => res.send("hello there"));
 
 app.use(authRouter);
+app.use("/react", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 module.exports = app;
