c403f4480a9b7c7ca651564f6ba4cdc26a227624
Stanley Huang <huang.stanley050@gmail.com>
2020-01-11 14:52:10 +1100

    some more funny path issue

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ const authRouter = require("./routes/authRoutes");
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) => res.send("Index route"));
 app.get("/hello", (req, res) => res.send("hello there"));
-
 app.use(authRouter);
-app.use("/react", (req, res) => {
+
+app.get("/react", (req, res) => {
   res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
