59423b278e9b17ec2c9fde9b2b46dd93a890bc30
Stanley Huang <huang.stanley050@gmail.com>
2020-01-11 15:01:01 +1100

    trying something else

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const app = express();
 const authRouter = require("./routes/authRoutes");
 
 app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("/", (req, res) => res.send("Index route"));
+//app.get("/", (req, res) => res.send("Index route"));
 app.get("/hello", (req, res) => res.send("hello there"));
 app.use(authRouter);
 
-app.get("/react", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
